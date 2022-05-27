0022e742141ae15dc69dafa9d57a1bb356b5c954
dingdi <dingd@meixing.com>
2022-05-27 16:41:50 +0800

    启动页

--- app/src/main/java/com/dd/cloudmusic/widget/TopBar.kt
+++ app/src/main/java/com/dd/cloudmusic/widget/TopBar.kt
@@ -1,6 +1,18 @@
 package com.dd.cloudmusic.widget
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.dd.cloudmusic.R
 
 @Composable
 fun SearchBar(
@@ -11,7 +23,23 @@ fun SearchBar(
 
 @Composable
 fun TitleBar(
+    iconResource:Int?,
     onIconClick: () -> Unit,
+    title :String?
 ) {
-
+    Row() {
+        Icon(
+            //默认显示返回图标
+            painter = painterResource( id = iconResource?: R.mipmap.icon_back),
+            contentDescription = "用户",
+            modifier = Modifier
+                .size(25.dp)
+                .padding(start = 10.dp)
+                .align(Alignment.CenterVertically)
+                .clickable {
+                    onIconClick.invoke()
+                }
+        )
+        Text(text = "$title",textAlign = TextAlign.Center)
+    }
 }
