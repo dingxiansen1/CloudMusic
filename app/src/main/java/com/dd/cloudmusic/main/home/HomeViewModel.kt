855b7a60b63ed481cca2beff6688701ae4d6717c
dingdi <dingd@meixing.com>
2022-05-31 11:07:45 +0800

    启动页

--- app/src/main/java/com/dd/cloudmusic/main/home/HomeViewModel.kt
+++ app/src/main/java/com/dd/cloudmusic/main/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.dd.cloudmusic.main.home
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -19,30 +20,26 @@ class HomeViewModel @Inject constructor(
     private var service: HttpService,
 ) : BaseViewModel() {
 
+    var viewStates by mutableStateOf(HomeViewState())
+        private set
+
     init {
         getData()
     }
 
-    var viewStates by mutableStateOf(HomeViewState())
-        private set
-
-    /**
-     *  轮播
-     * */
-    private val bannerFlow = flow {
-        emit(service.getBanner())
-    }.map {
-        it.banners ?: emptyList()
-    }
-    /**
-     *  首页Icon
-     * */
-    private val homeIconFlow = flow {
-        emit(service.getHomeIcon())
-    }.map {
-        it.data ?: emptyList()
-    }
     private fun getData() {
+        // 轮播
+        val bannerFlow = flow {
+            emit(service.getBanner())
+        }.map {
+            it.banners ?: emptyList()
+        }
+        // 首页Icon
+        val homeIconFlow = flow {
+            emit(service.getHomeIcon())
+        }.map {
+            it.data ?: emptyList()
+        }
         viewModelScope.launch {
             bannerFlow.zip(homeIconFlow){banners, icons ->
                 viewStates =
