0d8867c602e41bf7e1bfcd15d081470b471c6ce6
dingdi <dingd@meixing.com>
2022-06-01 09:10:56 +0800

    首页

--- app/src/main/java/com/dd/cloudmusic/main/home/HomePage.kt
+++ app/src/main/java/com/dd/cloudmusic/main/home/HomePage.kt
@@ -1,14 +1,24 @@
 package com.dd.cloudmusic.main.home
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -17,6 +27,9 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.RoundedCornersTransformation
 import com.dd.base.ext.showToast
 import com.dd.cloudmusic.R
 import com.dd.cloudmusic.bean.Creative
@@ -69,6 +82,24 @@ fun HomePage(
             }
         }
         if (recommendPlayList.isNotEmpty()) {
+            Box(Modifier.fillMaxWidth().height(50.dp)) {
+                Text(
+                    text = "推荐歌单",
+                    fontSize = 18.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 20.dp)
+                )
+
+                IconButton(
+                    onClick = { showToast("暂未开发") }, Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 20.dp)
+                ) {
+                    Text(text = "更多", fontSize = 18.sp)
+                    Icon(painter = painterResource(id = R.mipmap.icon_back_right), contentDescription = "更多")
+                }
+            }
             LazyRow {
                 items(recommendPlayList.size) { index ->
                     HomeRecommendPage(recommendPlayList[index])
@@ -98,28 +129,27 @@ fun HomeIconPage(data: HomeIconBean) {
 
 @Composable
 fun HomeRecommendPage(data: Creative) {
-    Box(
+    Column(
         modifier = Modifier
             .height(150.dp)
             .width(150.dp)
-            .padding(10.dp)
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         AsyncImage(
             model = data.uiElement.image.imageUrl,
             contentDescription = data.uiElement.mainTitle.title,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(10.dp)),
         )
         Text(
             text = data.uiElement.mainTitle.title,
             fontSize = 16.sp,
             modifier = Modifier
                 .fillMaxWidth()
-                .align(
-                    Alignment.BottomCenter
-                ),
+                .padding(10.dp),
             maxLines = 2,
             overflow = TextOverflow.Ellipsis,
-            style = TextStyle(color = AppTheme.colors.white)
+            style = TextStyle(color = AppTheme.colors.textPrimary)
         )
     }
 }
